ce1a0ea12995a8706835dd78602f0039ddedbc90
C-YooJin <ioi_3312@naver.com>
2020-12-24 16:06:42 +0900

    Add driver option, Logger

---
+++ src/main/java/com/deepjin/larva/Crawler.java
@@ -0,0 +1,65 @@
+package com.deepjin.larva;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Crawler {
+    public static void main(String[] args) {
+
+        Crawler crawler = new Crawler();
+        crawler.crawl();
+
+    }
+
+    //WebDriver
+    private WebDriver driver;
+
+    // Logger
+    private static final Logger logger = LoggerFactory.getLogger(Crawler.class);
+
+    //Properties
+    public static final String WEB_DRIVER_ID = "webdriver.chrome.driver";
+    public static final String WEB_DRIVER_PATH = "/Users/user/Documents/etc/chromedriver";
+
+
+    //크롤링 할 URL
+    private String base_url;
+
+    public Crawler() {
+        super();
+
+        //System Property SetUp
+        System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
+
+        // Options for driver
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--disable-popup-blocking");   // 팝업창 무시
+
+        //Driver SetUp
+        driver = new ChromeDriver();
+        base_url = "https://www.naver.com";
+    }
+
+    public void crawl() {
+
+        try {
+            //get page (= 브라우저에서 url을 주소창에 넣은 후 request 한 것과 같다)
+            driver.get(base_url);
+            System.out.println(driver.getPageSource());
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        } finally {
+
+            driver.close();
+        }
+
+    }
+
+}
